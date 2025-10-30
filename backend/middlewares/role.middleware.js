355863b36687a1aaa5a1d814fc7432b315c5dcef
Demiana Ayman <133611525+Demi-ayman@users.noreply.github.com>
2025-10-30 07:45:45 +0300

    Delete backend directory

--- backend/middlewares/role.middleware.js
+++
@@ -1,9 +0,0 @@
-exports.authorize = (...allowedRules)=>{
-  return (req,res,next)=>{
-    const userRole = req.user.role
-    if(!allowedRules.includes(userRole)){
-      return res.status(403).json({message:'access denied'})
-    }
-    return next();
-  }
-}
